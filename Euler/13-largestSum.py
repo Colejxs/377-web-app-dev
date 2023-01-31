469f02494cf58df575ad0a17cfdc4bd2f883bcbf
Colejxs <cspencer24@hanoverstudents.org>
2023-01-31 18:26:59 -0500

    final euler

--- Euler/13-largestSum.py
+++ Euler/13-largestSum.py
@@ -1,11 +1,13 @@
 runningTotal = 0
+
 file = open('13-largestSum.dat', 'r')
 lines = file.readlines()
+
 for line in lines:
         number = int(line.strip())
         runningTotal += number
+
 newTotal = str(runningTotal)
-for element in newTotal:
-    print(element)
+print(newTotal[:10])
     
 
--- Euler/7-1001Prime.py
+++ Euler/7-1001Prime.py
@@ -1,7 +1,25 @@
-for i in range (2, 10):
-    for j in range(i):
-        if i % j == 0:
-            #is not a prime factor 
-            break
-        else:
-            print(i)
+import math
+primes = [2]
+# for i in range (3, 10002):
+#     for j in range(2, i):
+
+#         if i % j == 0:
+#             #is not a prime factor 
+#             continue
+#         else:
+#             primes.append(i)
+
+def isPrime(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False 
+    return True
+
+i = 3
+while len(primes) < 10_001:
+    if(isPrime(i)):
+        primes.append(i)
+    i += 2
+
+print(primes[10000])
+
